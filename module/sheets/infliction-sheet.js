497c2d975cc3715161a72bbaf16f1e6a0fd721d5
Hangmansgate <hangmansgaterpg@gmail.com>
2026-04-24 13:12:03 -0400

    initial local repo setup

---
+++ module/sheets/infliction-sheet.js
@@ -0,0 +1,33 @@
+export class LocusInflictionSheet extends ItemSheet {
+  static get defaultOptions() {
+    return foundry.utils.mergeObject(super.defaultOptions, {
+      classes: ["locus-system", "sheet", "item", "infliction"],
+      template: "systems/locus-system/templates/item/infliction-sheet.html",
+      width: 620,
+      height: 620,
+      resizable: true
+    });
+  }
+
+  get title() {
+    return `${this.item.name} — Infliction`;
+  }
+
+  getData(options = {}) {
+    const context = super.getData(options);
+    context.system = this.item.system;
+    context.isInfliction = this.item.type === "infliction";
+    context.difficulties = ["easy", "medium", "hard"];
+    context.attributes = [
+      "frailty",
+      "clumsiness",
+      "carelessness",
+      "impatience",
+      "cowardice",
+      "ignorance",
+      "repulsion",
+      "temper"
+    ];
+    return context;
+  }
+}
