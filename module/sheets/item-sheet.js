1ae98f5621ceb0de71b069c574b633aee59aae0e
Hangmansgate <hangmansgaterpg@gmail.com>
2026-04-24 14:10:08 -0400

    Fix remaining merge conflicts and restore custom sheets

--- module/sheets/item-sheet.js
+++ module/sheets/item-sheet.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 export class LocusItemSheet extends ItemSheet {
   static get defaultOptions() {
     return foundry.utils.mergeObject(super.defaultOptions, {
@@ -20,27 +19,3 @@ export class LocusItemSheet extends ItemSheet {
     context.isGenericItem = this.item.type === "item";
     return context;
   }
-=======
-export class LocusItemSheet extends ItemSheet {
-  static get defaultOptions() {
-    return foundry.utils.mergeObject(super.defaultOptions, {
-      classes: ["locus-system", "sheet", "item"],
-      template: "systems/locus-system/templates/item/item-sheet.html",
-      width: 560,
-      height: 520,
-      resizable: true
-    });
-  }
-
-  get title() {
-    return `${this.item.name} — Item`;
-  }
-
-  getData(options = {}) {
-    const context = super.getData(options);
-    context.system = this.item.system;
-    context.isGenericItem = this.item.type === "item";
-    return context;
-  }
->>>>>>> 60a8c33e340506e04a93be96f090995ec22a958d
-}
